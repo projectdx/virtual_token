9c4b112e20bc5464e083257af21816a0d0ff7d27
Ravi Gadad <ravi@renewfund.com>
2015-03-11 02:52:49 -0700

    Set domain for sending production email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'virtual-token.heroku.com',
     :enable_starttls_auto => true
   }
 end
